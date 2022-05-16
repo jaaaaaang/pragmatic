314796d5286a0a891ec6b11a374a53d27a6ca645
장영우님(WILLIAM)/Infra AI/DT추진팀 <jaaaang.yw@gmail.com>
2022-05-16 20:38:45 +0900

    initial commit

---
+++ accountapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from accountapp.views import hello_world
+
+
+#"127.0.0.1:8000/account/hello_world"  <-- 이렇게 들어와야 하는데
+#app_name 을 만들어 줌으로써  접급이 용이해짐... 나중에 ..
+
+app_name = 'accountapp'
+
+urlpatterns = [
+    path('hello_world/', hello_world, name = 'hello_world')
+
+]
