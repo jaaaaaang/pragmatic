e2938e00976f62962ce7cf413d5e2eac5ac6b496
장영우님(WILLIAM)/Infra AI/DT추진팀 <jaaaang.yw@gmail.com>
2022-05-17 10:28:33 +0900

    dj course 9

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from accountapp.views import hello_world
-
+from accountapp.views import test
 
 #"127.0.0.1:8000/account/hello_world"  <-- 이렇게 들어와야 하는데
 #app_name 을 만들어 줌으로써  접급이 용이해짐... 나중에 ..
@@ -9,6 +9,7 @@ from accountapp.views import hello_world
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('hello_world/', hello_world, name = 'hello_world')
-
+    path('hello_world/', hello_world, name = 'hello_world'),
+    #path('', hello_world, name = 'hello_world'),
+    path('test/', test, name = 'test')
 ]
