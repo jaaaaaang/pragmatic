4abce71519b9279b396615d6c89d570bc9a47df2
장영우님(WILLIAM)/Infra AI/DT추진팀 <jaaaang.yw@gmail.com>
2022-05-18 21:36:33 +0900

    dj course 21 sign up

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-
-from accountapp.views import hello_world
-from accountapp.views import test
+from accountapp.views import hello_world, test, AccountCreateView
 
 #"127.0.0.1:8000/account/hello_world"  <-- 이렇게 들어와야 하는데
 #app_name 을 만들어 줌으로써  접급이 용이해짐... 나중에 ..
@@ -11,5 +9,6 @@ app_name = 'accountapp'
 urlpatterns = [
     path('hello_world/', hello_world, name = 'hello_world'),
     #path('', hello_world, name = 'hello_world'),
-    path('test/', test, name = 'test')
+    path('test/', test, name = 'test'),
+    path('create/', AccountCreateView.as_view(), name='create'),  #view.py에서 class를 사용한 경우 .as_view()를 붙여주라
 ]
