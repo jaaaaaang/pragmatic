b70f45cbd8f343c526d2475c6191db17f0215ed5
장영우님(WILLIAM)/Infra AI/DT추진팀 <jaaaang.yw@gmail.com>
2022-05-23 23:14:33 +0900

    my custom_add HTML

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from accountapp.views import hello_world, test, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, test, AccountCreateView, AccountDetailView, AccountUpdateView, trafficmap, trmap
 
 #"127.0.0.1:8000/account/hello_world"  <-- 이렇게 들어와야 하는데
 #app_name 을 만들어 줌으로써  접급이 용이해짐... 나중에 ..
@@ -10,7 +10,11 @@ app_name = 'accountapp'
 urlpatterns = [
     path('hello_world/', hello_world, name = 'hello_world'),
     #path('', hello_world, name = 'hello_world'),
+
     path('test/', test, name = 'test'),
+    path('trafficmap/', trafficmap, name = 'trafficmap'),
+    path('trmap/', trmap, name='trmap'),
+
 
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'), #template 설정을 해줘야 함
     path('logout/', LogoutView.as_view(), name='logout'),
@@ -18,4 +22,5 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),  #view.py에서 class를 사용한 경우 .as_view()를 붙여주라
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -11,29 +11,39 @@ from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
-def hello_world(request):
 
-    if request.method=="POST":
+def hello_world(request):
+    if request.method == "POST":
         temp = request.POST.get('hello_world_input')
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
-
 
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
+
 # 내가 걍 이해하기 위해서 만들어본 것
 def test(request):
-   return render(request, 'test.html')
+    return render(request, 'test.html')
+
+def trafficmap(request):
+    return render(request, 'trafficmap.html')
+
+def trmap(request):
+    return render(request, 'accountapp/trmap.html')
+
+
+# 수업내역
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
 
 class AccountDetailView(DetailView):
     model = User
